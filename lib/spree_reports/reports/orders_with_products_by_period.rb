2dc56b8977429e4192eebb137d6ca9936309f23d
Jonathan Garay <jonathan@devmask.net>
2015-08-18 20:35:17 -0600

    * The orders_with_products_by_period now returns the parts as apart   of the list

--- lib/spree_reports/reports/orders_with_products_by_period.rb
+++ lib/spree_reports/reports/orders_with_products_by_period.rb
@@ -17,34 +17,52 @@ module SpreeReports
         @currencies = Spree::Order.select('currency').distinct.map { |c| c.currency }
         @currency = @currencies.include?(params[:currency]) ? params[:currency] : @currencies.first
 
-        @stores = Spree::Store.all.map { |store| [store.name, store.id] }
-        @stores << ["all", "all"]
+        @stores = Spree::Store.pluck(:name, :id)
+        @stores << %w(all all)
+
         @store = @stores.map{ |s| s[1].to_s }.include?(params[:store]) ? params[:store] : @stores.first[1]
 
-        @date_start = Date.parse(params[:start]).strftime("%Y-%m-%d")
-        @date_end = Date.parse(params[:end]).strftime("%Y-%m-%d")
+        @date_start = Date.parse(params[:start]).strftime('%Y-%m-%d')
+        @date_end = Date.parse(params[:end]).strftime('%Y-%m-%d')
 
       end
 
       def get_data
-
-        @line_items = Spree::LineItem.includes(:order).where("created_at > ?", @date_start).where("created_at < ?", @date_end)
-
+        @line_items = Spree::LineItem
+                          .includes( :order, :variant, {assembly_variants: :variant})
+                          .where(created_at: @date_start..@date_end)
       end
 
       def build_response
+        parts = []
+
         @data = @line_items.map do  |l|
+          parts << l.assembly_variants.map{|av| present_part(l, av.variant)} unless l.assembly_variants.empty?
+
           {
             date: l.created_at,
             sku: l.variant.sku,
             shipping_price: "",
             discounts: l.promo_total,
             unit_price: l.cost_price,
-            revenue: l.cost_price - l.promo_total,
-            order: l.order.number
+            revenue: (l.cost_price || 0) - (l.promo_total || 0),
+            order: l.order.number,
           }
-
         end
+
+        @data.concat parts
+      end
+
+      def present_part(line_item, item)
+        {
+            date: item.updated_at,
+            sku: item.sku,
+            shipping_price: "",
+            discounts: line_item.promo_total,
+            unit_price: item.cost_price,
+            revenue: item.cost_price,
+            order: line_item.order.number
+        }
       end
 
       def to_csv
